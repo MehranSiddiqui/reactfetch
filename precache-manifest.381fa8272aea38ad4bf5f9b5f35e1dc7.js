0db11097c2c1b98daf51203a28f68f21565436dd
MehranSiddiqui <mmehran0703@yahoo.in>
2022-01-23 12:49:05 +0530

    Updates

---
+++ precache-manifest.381fa8272aea38ad4bf5f9b5f35e1dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a4315c0ddd66c12138fa277786fc50",
+    "url": "/reactfetch/index.html"
+  },
+  {
+    "revision": "bcf21a39c8dbcfa5db69",
+    "url": "/reactfetch/static/css/main.d4c30e10.chunk.css"
+  },
+  {
+    "revision": "42001a2fcf70f5d54b93",
+    "url": "/reactfetch/static/js/2.5d0f8bfd.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/reactfetch/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "bcf21a39c8dbcfa5db69",
+    "url": "/reactfetch/static/js/main.f365e50e.chunk.js"
+  },
+  {
+    "revision": "02d744a6c6294b3305bf",
+    "url": "/reactfetch/static/js/runtime~main.68f31f51.js"
+  }
+]);
